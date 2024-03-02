b0a3c6ce65586f18553018bfeb726a2ed5c4bd90
AKASH J <akjaddu@gmail.com>
2024-03-02 16:41:28 +0530

    Backend of ML models are integrated through flask !

--- Medical-Backend/Chatbot-Predictions-Flask/main.py
+++ Medical-Backend/Chatbot-Predictions-Flask/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 from medAPI import getAPI_Bot
+from predictions import Diabetes, Heart
 
 app = Flask(__name__)
 CORS(app)
@@ -11,12 +12,74 @@ def home():
 
 @app.route("/chatAPI", methods=['POST'])
 def chatAPI():
-    body = request.get_json()
-    response= getAPI_Bot(body['user'])
-    response = response.replace("Invictus", "MedAi")
-    response = {"bot":response}
-    print(response)
-    return response
+    try:
+        body = request.get_json()
+        response= getAPI_Bot(body['user'])
+        response = response.replace("Invictus", "MedAi")
+        response = {"bot":response}
+        return response
+    except Exception as e:
+        return (str(e))
+
+@app.route("/diabetes-prediction", methods=['POST'])
+def diabetes_predict():
+    try:
+        data = request.get_json()
+        input_data = [
+            data['age'],
+            data['gender'],
+            data['polyuria'],
+            data['polydipsia'],
+            data['sudden_weight_loss'],
+            data['weakness'],
+            data['polyphagia'],
+            data['genital_thrush'],
+            data['visual_blurring'],
+            data['irritability'],
+            data['partial_paresis'],
+            data['alopecia']
+        ]
+        # Convert the input data to a NumPy array
+        input_array = [float(feature) for feature in input_data]
+        input_array = [input_array]
+        # Call the Diabetes function to get the prediction
+        result = Diabetes(input_array)
+        # print("prediction:", result)
+        return (result)
+
+    except Exception as e:
+        print('error', str(e))
+        return(str(e))
+    
+@app.route("/heart-disease-prediction", methods=['POST'])
+def heart_predict():
+    try:
+        data = request.get_json()
+        input_data = [
+            data['age'],
+            data['sex'],
+            data['cp'],
+            data['trestbps'],
+            data['chol'],
+            data['fbs'],
+            data['restecg'],
+            data['thalach'],
+            data['exang'],
+            data['oldpeak'],
+            data['slope'],
+            data['ca'],
+            data['thal']
+        ]
+        # Convert the input data to a NumPy array
+        input_array = [float(feature) for feature in input_data]
+        input_array = [input_array]
+        # Call the Diabetes function to get the prediction
+        result = Heart(input_array)
+        # print("prediction:", result)
+        return (result)
+
+    except Exception as e:
+        return(str(e))
 
 if __name__ == "__main__":
     app.run(host="localhost",port = 8000, debug = True)
---
+++ Medical-Backend/Chatbot-Predictions-Flask/predictions.py
@@ -0,0 +1,86 @@
+import numpy as np
+import os
+import pandas as pd
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+import cv2
+import tensorflow 
+from tensorflow.keras.preprocessing.image import array_to_img, img_to_array, load_img
+from tensorflow.keras.preprocessing import image
+from keras.models import load_model
+import pickle
+
+
+# X------------------------------------------------------------------------------------X
+diabetes_model_path = r"predictions\Machine Learning\models\diabetes-dt-model.pkl"
+heart_model_path = r"predictions\Machine Learning\models\heart-rf-model.pkl"
+# pneumonia_model_path = r"predictions\Neural Nets\Models\pneumonia\resnet-pneumonia.h5"
+predictions_path = os.path.abspath(__file__)
+# print("predictions.py file location:", predictions_path)
+
+# X------------------------------------------------------------------------------------X
+def Diabetes(input_data):
+    # Assuming 'predictions.py' is in 'Chatbot-Predictions-Flask' directory
+    with open(os.path.join(os.path.dirname(predictions_path), diabetes_model_path), 'rb') as file:
+        model = pickle.load(file)
+        print("Diabetes Model Loaded")
+    
+    # input_data = np.array([[58, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0]])  # Converted to a NumPy array
+
+    # Standardize the data (if needed, provide the scaler object)
+    # std_data = scaler.transform(input_data)
+
+    prediction = model.predict(input_data)
+    print(prediction)
+    if prediction[0] == 0:
+        return("The Person does not have diabetes")
+    else:
+        return("The Person have diabetes")
+
+# X------------------------------------------------------------------------------------X
+def Heart(input_data):
+    # Assuming 'predictions.py' is in 'Chatbot-Predictions-Flask' directory
+    with open(os.path.join(os.path.dirname(predictions_path), heart_model_path), 'rb') as file:
+        model = pickle.load(file)
+        print("Heart Model Loaded")
+    
+    # input_data = np.array([[57,0,0,120,354,0,1,163,1,0.6,2,0,2]])  # Converted to a NumPy array
+
+    # Standardize the data (if needed, provide the scaler object)
+    # std_data = scaler.transform(input_data)
+
+    prediction = model.predict(input_data)
+    print(prediction)
+    if prediction[0] == 0:
+        return("The Person does not have Heart disease")
+    else:
+        return("The Person have Heart disease")
+
+# X------------------------------------------------------------------------------------X
+# def Pneumonia():
+#     new_image_path = r"C:\Users\AKASH J\Desktop\testing\pneumonia\Testing-Data\Normal-Sample-XRay\NORMAL  (4).jpeg"
+#     # with open(os.path.join(os.path.dirname(predictions_path), pneumonia_model_path), 'rb') as file:
+#     pneumonia_model_path = r"C:\Users\AKASH J\Desktop\MedAPP\Medical-Backend\Chatbot-Predictions-Flask\predictions\Neural Nets\Models\pneumonia"
+#     # model = TFSMLayer(pneumonia_model_path, call_endpoint='serving_default')
+#     print("Pneumonia model loaded")
+
+#     try:
+#         # Load and preprocess the image
+#         img = image.load_img(new_image_path, target_size=(224,224))
+#         img_array = image.img_to_array(img)
+#         img_array = np.expand_dims(img_array, axis=0)
+#         img_array /= 255.0  # Normalize pixel values
+
+#         # Make predictions
+#         predictions = model.predict(img_array)
+
+#         # Interpret the result
+#         prediction_result = "PNEUMONIA" if predictions[0, 0] > 0.5 else "NORMAL"
+#         print("Prediction Result:", prediction_result)
+
+#     except Exception as e:
+#         print("Error:", str(e))
+
+# Pneumonia()        
